d638363b4ccbd73b496c99dbb613d336ecc576dc
sonia panisello <soniafrarma@hotmail.com>
2020-04-09 17:38:23 +0200

    affichage de la map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('test');
+
+console.log("hello");
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
+
+window.initMap = initMap; //rattache la fonction au scope global
